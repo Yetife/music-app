f854c14301ee4437b3b6d73a6485ebf7d69cab2d
Yetife <tifetreats21@gmail.com>
2022-09-16 09:36:02 +0100

    New files

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/app";
 import "firebase/auth"
 import 'firebase/firestore'
+import 'firebase/storage'
 
 const firebaseConfig = {
     apiKey: "AIzaSyC4E93xi7WgYAhHovuQx_J0JcrcXu4faps",
@@ -15,9 +16,11 @@ firebase.initializeApp(firebaseConfig)
 
 const auth = firebase.auth();
 const db = firebase.firestore()
+const storage = firebase.storage();
 
 const usersCollection = db.collection('users')
+const songsCollection = db.collection('songs')
 
 export{
-    auth, db, usersCollection
+    auth, db, usersCollection, storage, songsCollection
 }
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,58 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from "@/views/HomeView.vue"
+import AboutView from "@/views/AboutView.vue"
+import Manage from "../views/Manage.vue";
+import useUserStore from '../stores/user'
+
+const routes = [
+    {
+      name: "home",
+      path: "/",
+      component: HomeView,
+    },
+  {
+      name: "about",
+      path: "/about",
+    component: AboutView,
+  },
+    {
+        name: "manage",
+        path: "/manage-music",
+        component: Manage,
+        beforeEnter: (to, from, next) => {
+            next()
+        },
+        meta: {
+            requiresAuth: true,
+        }
+    },
+    {
+        path: '/manage',
+        redirect: {name : 'manage'}
+    },
+    {
+        path: '/:catchAll(.*)*',
+        redirect: {name : 'home'}
+    }
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+  routes,
+    linkExactActiveClass: "text-yellow-600"
 })
+router.beforeEach((to, from, next) => {
+    // console.log(to.meta)
 
+    if(!to.meta.requiresAuth){
+        next()
+        return;
+    }
+    const store = useUserStore()
+    if (store.userLoggedIn){
+        next()
+    }else{
+        next({name: "home"})
+    }
+})
 export default router
