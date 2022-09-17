774c65fa55353bef8c525e1692ba3f48d9aaea19
Yetife <tifetreats21@gmail.com>
2022-09-17 13:20:39 +0100

    New files

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -55,6 +55,7 @@ export default {
       uploads: [],
     }
   },
+  props: ['addSong'],
   methods: {
     upload($event){
       this.is_dragover = false
@@ -99,7 +100,10 @@ export default {
           };
 
           song.url = await task.snapshot.ref.getDownloadURL()
-          await songsCollection.add(song)
+          const songRef = await songsCollection.add(song)
+          const songSnapshot = await songRef.get()
+
+          this.addSong(songSnapshot)
 
           this.uploads[uploadIndex].variant = 'bg-green-400';
           this.uploads[uploadIndex].icon = 'fas fa-check';
@@ -107,7 +111,19 @@ export default {
         })
       })
       console.log(files)
-    }
+    },
+
+    //using refs to cancel a task
+  //   cancelUploads(){
+  //     this.uploads.forEach((upload) => {
+  //           upload.task.cancel();
+  //         })
+  //   }
+  },
+  beforeUnmount() {
+    this.uploads.forEach((upload) => {
+      upload.task.cancel();
+    })
   }
 }
 </script>
