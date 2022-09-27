f863c7409a2b68f5a7f3e0c90e8d2e8b46ac81bc
Yetife <tifetreats21@gmail.com>
2022-09-27 16:33:24 +0100

    New files

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -3,13 +3,22 @@ import "firebase/auth";
 import "firebase/firestore";
 import "firebase/storage";
 
+// const firebaseConfig = {
+//     apiKey: "AIzaSyC4E93xi7WgYAhHovuQx_J0JcrcXu4faps",
+//     authDomain: "music-app-de230.firebaseapp.com",
+//     projectId: "music-app-de230",
+//     storageBucket: "music-app-de230.appspot.com",
+//     appId: "1:648702781981:web:5b93e1631a4d038426f4f4"
+// };
+
 const firebaseConfig = {
-    apiKey: "AIzaSyC4E93xi7WgYAhHovuQx_J0JcrcXu4faps",
-    authDomain: "music-app-de230.firebaseapp.com",
-    projectId: "music-app-de230",
-    storageBucket: "music-app-de230.appspot.com",
-    appId: "1:648702781981:web:5b93e1631a4d038426f4f4"
+    apiKey: "AIzaSyBZu5qZjf0XZpPyIXIvUjJA5uCq8ak-tas",
+    authDomain: "music-upload-app.firebaseapp.com",
+    projectId: "music-upload-app",
+    storageBucket: "music-upload-app.appspot.com",
+    appId: "1:886275185425:web:2dd5d4ef955b6f8d944ab0",
 };
+
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
